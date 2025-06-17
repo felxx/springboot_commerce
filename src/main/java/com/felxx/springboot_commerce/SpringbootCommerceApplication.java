e5a2b8b4e40bcf439d63d6aa7ad20d92421755fd
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-17 14:02:47 -0300

    project created

---
+++ src/main/java/com/felxx/springboot_commerce/SpringbootCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.felxx.springboot_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felxx/springboot_commerce/SpringbootCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felxx.springboot_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
