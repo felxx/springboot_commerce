f79ec46c76f6f585543b02b93709e58b12d6ad7b
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-20 13:52:49 -0300

    feat: implement subtotal and total method

--- src/main/java/com/felxx/springboot_commerce/entities/Payment.java
+++ src/main/java/com/felxx/springboot_commerce/entities/Payment.java
@@ -3,6 +3,8 @@ package com.felxx.springboot_commerce.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -20,6 +22,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
