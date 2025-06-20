711068b633571d798b22b1daf13dafcfdf8ccdbd
felxx <igorfelipemonteirocontato@gmail.com>
2025-06-20 14:31:19 -0300

    feat: implement database exception

--- src/main/java/com/felxx/springboot_commerce/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/felxx/springboot_commerce/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.felxx.springboot_commerce.services.exceptions.DatabaseException;
 import com.felxx.springboot_commerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,5 +19,11 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         StandardError err = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not found", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        StandardError err = new StandardError(Instant.now(), HttpStatus.BAD_REQUEST.value(), "Database error", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/felxx/springboot_commerce/services/UserService.java
+++ src/main/java/com/felxx/springboot_commerce/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.felxx.springboot_commerce.entities.User;
 import com.felxx.springboot_commerce.repositories.UserRepository;
+import com.felxx.springboot_commerce.services.exceptions.DatabaseException;
 import com.felxx.springboot_commerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -30,13 +35,23 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 
     public User update(Long id, User user) {
-        User existingUser = userRepository.getReferenceById(id);
-        updateData(existingUser, user);
-        return userRepository.save(existingUser);
+        try{
+            User existingUser = userRepository.getReferenceById(id);
+            updateData(existingUser, user);
+            return userRepository.save(existingUser);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User existingUser, User user) {
